e7c8522c49b893933ccf5fe150081fb7205293e4
im25-pawlowskiv <pawlowskiv@bzz.ch>
2026-03-03 11:20:39 +0100

    Start

--- design.py
+++ design.py
@@ -1,3 +1,16 @@
 """
 This File is for the desing of the game
 """
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Hangman")
+root.geometry("400x300")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
+
+root.mainloop()
