04b23694517ed497c3a6d4a5f634fb2a11fb644c
im25-pawlowskiv <pawlowskiv@bzz.ch>
2026-03-03 11:19:34 +0100

    Start

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-"""
-This is the main File
-"""
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Hangman")
+root.geometry("400x300")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
+
+root.mainloop()
