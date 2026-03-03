64d034ce400d482eb5b0d491008537b6d62c0a4e
im25a-meyera2 <meyera2@bzz.ch>
2026-03-03 11:29:45 +0100

    Some more Files

--- design.py
+++ design.py
@@ -7,10 +7,24 @@ root = tk.Tk()
 root.title("Hangman")
 root.geometry("400x300")
 
-def main():
+
+
+
+
+def key_board_normal():
     pass
 
-if __name__ == '__main__':
+def wrong_letter():
     pass
+
+
+def false_postionn():
+    pass
+
+
+def right_letter():
+    pass
+
+
 
 root.mainloop()
