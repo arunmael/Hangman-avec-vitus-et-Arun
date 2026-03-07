bd7ba2c633c0bfcb5040f32ff3e2b1a67a662633
im25-pawlowskiv <pawlowskiv@bzz.ch>
2026-03-07 19:29:37 +0100

    Hangman Design gemacht mit einer einfacher logik requirements.txt gemacht --> zeigt welche module installiert wurden Die Hangman Fotos importiert

--- design.py
+++ design.py
@@ -1,16 +1,54 @@
 """
-This File is for the desing of the game
+This File is for the designing of the game
 """
 import tkinter as tk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Hangman")
-root.geometry("400x300")
+root.geometry("300x400")
+
+canvas = tk.Canvas(root, width=300, height=300, bg="black")
+canvas.grid(row=0, column=0)
+
+# ========== LOAD PICTURES ==========
+
+hangman = [] # wird gebraucht um das PNG zu
+for i in range(1, 14):
+    img = Image.open(f"hangman_images/stage{i}.png")
+    img = img.resize((300, 300))
+    photo = ImageTk.PhotoImage(img)
+    hangman.append(photo)
 
 
-#Test
-button = tk.Button(root, text="A")
-button.pack()
+
+image_ids = []
+count = 0
+
+# ========== FUNCTIONS ==========
+
+def draw_stage():
+    global count
+    created_image = canvas.create_image(150, 150, image=hangman[count])
+    image_ids.append(created_image)
+    count += 1
+
+def remove_stage():
+    global count
+    last_image = image_ids.pop()
+    canvas.delete(last_image)
+    count -= 1
+
+# ========== Buttons ==========
+
+btn_frame = tk.Frame(root)
+btn_frame.grid(row=1, column=0)
+
+backward_btn = tk.Button(btn_frame, text="Back", command=remove_stage) # removes hangman stage
+forward_btn = tk.Button(btn_frame, text="Forward", command=draw_stage) # adds hangman stage
+backward_btn.grid(row=1, column=0)
+forward_btn.grid(row=1, column=1)
+
 
 
 def key_board_normal():
